f1333ed5f905612eeb601a03a91b05c258390b18
Vinicius Gomes Batista <vinicius.batista@bernoulli.com.br>
2025-05-26 13:25:20 -0300

    Atualização

--- snapshots/empresas_crm.sql
+++ snapshots/empresas_crm.sql
@@ -11,17 +11,18 @@
         'proprietario',
         'lideranca_regional',
         'lideranca_nacional'
-]
+      ]
     )
   }}
 
   SELECT
-  accountid,
-  [Área de Atuação] AS area_de_atuacao,
-  [Gerência Pedagógica Atual] AS gerencia_pedagogica_atual,
-  [Consultor Pedagógico Atual] AS consultor_pedagogico_atual,
-  [Proprietário] AS proprietario,
-  [Liderança Regional] AS lideranca_regional,
-  [Liderança Nacional] AS lideranca_nacional
-FROM {{ source('intel_merc','brz_empresas') }}
+    accountid,
+    [Área de Atuação] AS area_de_atuacao,
+    COALESCE([Gerência Pedagógica Atual], '') AS gerencia_pedagogica_atual,
+    COALESCE([Consultor Pedagógico Atual], '') AS consultor_pedagogico_atual,
+    LTRIM(RTRIM(COALESCE([Proprietário], ''))) AS proprietario,
+    LTRIM(RTRIM(COALESCE([Liderança Regional], ''))) AS lideranca_regional,
+    COALESCE([Liderança Nacional], '') AS lideranca_nacional
+  FROM {{ source('intel_merc','brz_empresas') }}
+
 {% endsnapshot %}
