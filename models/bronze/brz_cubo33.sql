67bceacfe06b459e7116b006f7f675bc4d104d7e
Vinicius Gomes Batista <vinicius.batista@bernoulli.com.br>
2025-07-23 11:09:01 -0300

    Atualização

--- models/bronze/brz_cubo33.sql
+++ models/bronze/brz_cubo33.sql
@@ -1,4 +1,5 @@
-{{ config(materialized='table') }}
+--{{ config(materialized='table') }}
 
 SELECT *
-FROM {{ source('operacoes', 'BI_VENDACOLECAO') }}
+--FROM {{ source('operacoes', 'BI_VENDACOLECAO') }}
+FROM {{ source('intel_merc', 'brz_vendacolecao_rm') }}
