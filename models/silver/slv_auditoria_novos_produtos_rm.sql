e1c56ea6fd40464513ae07a7c14c5f3804d8ce6f
Vinicius Gomes Batista <vinicius.batista@bernoulli.com.br>
2025-08-07 10:36:44 -0300

    Atualização

---
+++ models/silver/slv_auditoria_novos_produtos_rm.sql
@@ -0,0 +1,10 @@
+SELECT distinct [CÓDIGO PRODUTO]
+,[COLEÇÃO]
+,CATEGORIA
+,[PERNONALIZAÇÃO]
+FROM {{ ref('slv_cubo33_categorizado') }}
+WHERE [GRUPO COLEÇÃO] IS NULL
+	AND [ANO DE UTILIZAÇÃO] = '2025'
+    AND [ALUNO/PROFESSOR] = 'Aluno'
+    AND CODTMV NOT IN ('2.1.48', '2.2.76', '2.1.04')
+    AND [TIPO DE PEDIDO] IN ('Acerto de NF', 'Adicional', 'D', 'Tiragem')
