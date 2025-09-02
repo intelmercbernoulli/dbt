a614a86c82a9dfada0576d3bc7af0aa963c4551f
Vinicius Gomes Batista <vinicius.batista@bernoulli.com.br>
2025-09-02 11:04:16 -0300

    Atualização

--- models/silver/slv_empresas_com_cep.sql
+++ models/silver/slv_empresas_com_cep.sql
@@ -1,20 +1,21 @@
 SELECT 
-      e.[Nome Fantasia],
-      e.[Quantidade Total de Alunos],
-      e.[Rede de Ensino],
-      e.[Estado],
-      e.[Cidade],
-      e.[Bairro],
-      e.[CEP],
-      e.[Tipo de Relação],
-      e.[Código RM],
-      e.[Código INEP],
-      e.[Perfil da escola],
-      e.[CNPJ],
-      c.[latitude],
-      c.[longitude],
-      c.[centroide]
-FROM {{ source ('intel_merc', 'brz_empresas') }} e
-LEFT JOIN {{ source ('intel_merc', 'brz_base_cep') }} c
-       ON REPLACE(REPLACE(LTRIM(RTRIM(e.[CEP])),'-',''),' ','') = 
-          REPLACE(REPLACE(LTRIM(RTRIM(c.[cep])),'-',''),' ','')
+      e."Nome Fantasia",
+      e."Quantidade Total de Alunos",
+      e."Rede de Ensino",
+      e."Estado",
+      e."Cidade",
+      e."Bairro",
+      e."CEP",
+      e."Tipo de Relação",
+      e."Área de Atuação",
+      e."Código RM",
+      e."Código INEP",
+      e."Perfil da escola",
+      e."CNPJ",
+      c."latitude",
+      c."longitude",
+      c."centroide"
+FROM {{ source('intel_merc', 'brz_empresas') }} e
+LEFT JOIN {{ source('intel_merc', 'brz_base_cep') }} c
+       ON REPLACE(REPLACE(LTRIM(RTRIM(e."CEP")),'-',''),' ','') = 
+          REPLACE(REPLACE(LTRIM(RTRIM(c."cep")),'-',''),' ','')
