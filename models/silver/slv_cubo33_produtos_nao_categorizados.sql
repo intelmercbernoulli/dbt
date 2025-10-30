a89caf42486b795734e7fde69cf8fda8160d7648
Vinicius Gomes Batista <vinicius.batista@bernoulli.com.br>
2025-10-30 17:55:32 -0300

    Atualização

---
+++ models/silver/slv_cubo33_produtos_nao_categorizados.sql
@@ -0,0 +1,13 @@
+SELECT 
+    venda.*,
+    categorizacao.[SÉRIE],
+    categorizacao.[SEGMENTO],
+    categorizacao.[GRUPO COLEÇÃO],
+    categorizacao.[CATEGORIA PADRONIZADA]
+FROM 
+    {{ ref('slv_cubo33_aluno') }} AS venda
+LEFT JOIN
+    {{ source('intel_merc', 'brz_categorizacao_produtos') }} AS categorizacao 
+    ON venda.[COLEÇÃO] = categorizacao.[COLEÇÃO]
+WHERE 
+    categorizacao.[COLEÇÃO] IS NULL;
