38179837e12bec93da7c4c005e24602e986b8aae
Vinicius Gomes Batista <vinicius.batista@bernoulli.com.br>
2025-11-10 16:18:02 -0300

    Atualização

--- models/gold/gld_empresas_integracao_crm_mb.sql
+++ models/gold/gld_empresas_integracao_crm_mb.sql
@@ -26,32 +26,32 @@ with empresas as (
 ),
 
 alunos as (
-    select
-        [id_account],
-        COALESCE([EFAI 1º ano], 'Não') as [EFAI 1º ano],
-        COALESCE([EFAI 2º ano], 'Não') as [EFAI 2º ano],
-        COALESCE([EFAI 3º ano], 'Não') as [EFAI 3º ano],
-        COALESCE([EFAI 4º ano], 'Não') as [EFAI 4º ano],
-        COALESCE([EFAI 5º ano], 'Não') as [EFAI 5º ano],
-        COALESCE([EFAF 6º ano], 'Não') as [EFAF 6º ano],
-        COALESCE([EFAF 7º ano], 'Não') as [EFAF 7º ano],
-        COALESCE([EFAF 8º ano], 'Não') as [EFAF 8º ano],
-        COALESCE([EFAF 9º ano], 'Não') as [EFAF 9º ano],
-        COALESCE([EI 2 anos], 'Não') as [EI 2 anos],
-        COALESCE([EI 3 anos], 'Não') as [EI 3 anos],
-        COALESCE([EI 4 anos], 'Não') as [EI 4 anos],
-        COALESCE([EI 5 anos], 'Não') as [EI 5 anos],
-        COALESCE([EM 1ª série], 'Não') as [EM 1ª série],
-        COALESCE([EM 2ª série], 'Não') as [EM 2ª série],
-        COALESCE([EM 3ª série], 'Não') as [EM 3ª série],
-        COALESCE([Pré-vestibular anual], 'Não') as [Pré-vestibular anual],
-        COALESCE([Pré-vestibular semestral], 'Não') as [Pré-vestibular semestral]
+    select *
     from {{ ref('slv_aluno_potencial_integracao_crm_mb') }}
 )
 
 select
     e.*,
-    a.*
+    -- Aqui aplicamos COALESCE apenas nas colunas de interesse da tabela alunos
+    COALESCE(a.[EFAI 1º ano], 'Não') as [EFAI 1º ano],
+    COALESCE(a.[EFAI 2º ano], 'Não') as [EFAI 2º ano],
+    COALESCE(a.[EFAI 3º ano], 'Não') as [EFAI 3º ano],
+    COALESCE(a.[EFAI 4º ano], 'Não') as [EFAI 4º ano],
+    COALESCE(a.[EFAI 5º ano], 'Não') as [EFAI 5º ano],
+    COALESCE(a.[EFAF 6º ano], 'Não') as [EFAF 6º ano],
+    COALESCE(a.[EFAF 7º ano], 'Não') as [EFAF 7º ano],
+    COALESCE(a.[EFAF 8º ano], 'Não') as [EFAF 8º ano],
+    COALESCE(a.[EFAF 9º ano], 'Não') as [EFAF 9º ano],
+    COALESCE(a.[EI 2 anos], 'Não') as [EI 2 anos],
+    COALESCE(a.[EI 3 anos], 'Não') as [EI 3 anos],
+    COALESCE(a.[EI 4 anos], 'Não') as [EI 4 anos],
+    COALESCE(a.[EI 5 anos], 'Não') as [EI 5 anos],
+    COALESCE(a.[EM 1ª série], 'Não') as [EM 1ª série],
+    COALESCE(a.[EM 2ª série], 'Não') as [EM 2ª série],
+    COALESCE(a.[EM 3ª série], 'Não') as [EM 3ª série],
+    COALESCE(a.[Pré-vestibular anual], 'Não') as [Pré-vestibular anual],
+    COALESCE(a.[Pré-vestibular semestral], 'Não') as [Pré-vestibular semestral]
+
 from empresas e
 left join alunos a
     on e.accountid = a.id_account
