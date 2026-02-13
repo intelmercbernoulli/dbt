1df7d7129c413b26d45c4f1459cc524f62c4786f
Vinicius Gomes Batista <vinicius.batista@bernoulli.com.br>
2026-02-13 10:25:16 -0300

    Criação da consulta todos produtos 2026.

---
+++ models/gold/gld_2026_alunos_todos_produtos.sql
@@ -0,0 +1,32 @@
+SELECT 
+    [CÓDIGO DO CLIENTE],
+    [NOME DO CLIENTE],
+    [REDE DE ENSINO],
+    [CIDADE],
+    [UF],
+	SEGMENTO,
+    [SÉRIE],
+    [DESCRIÇÃO],
+    CATEGORIA,
+    [COLEÇÃO],
+    [GRUPO COLEÇÃO],
+    [CATEGORIA PADRONIZADA],
+    SUM([QUANTIDADE PEDIDO]) AS ALUNOS
+FROM {{ ref('slv_cubo33_categorizado') }}
+WHERE
+        ([ANO DE UTILIZAÇÃO] = '2026'
+        AND VOLUME IN ('1', 'Único', 'U')
+        AND [GRUPO COLEÇÃO] NOT IN ('CAMBRIDGE','COLEÇÃO PRINCIPAL'))
+    OR 
+        ([ANO DE UTILIZAÇÃO] = '2026'
+        AND VOLUME IN ('1', 'Único', 'U')
+        AND [GRUPO COLEÇÃO] = 'CAMBRIDGE'
+        AND ( [DESCRIÇÃO] LIKE '%STUDENT%'
+            OR [DESCRIÇÃO] LIKE '%STUDENTS%'
+            OR [DESCRIÇÃO] LIKE '%STUDENT''S%'
+            OR [DESCRIÇÃO] LIKE '%SB%'))
+    OR 
+        ([ANO DE UTILIZAÇÃO] = '2026'
+        AND CATEGORIA IN ('Coleção', 'Coleção - 1º Semestre', 'Coleção - 2º Semestre')
+        AND VOLUME IN ('1', 'Único', 'U'))
+GROUP BY [CÓDIGO DO CLIENTE],[NOME DO CLIENTE], [REDE DE ENSINO], [CIDADE], [UF],  SEGMENTO, [SÉRIE],[DESCRIÇÃO],CATEGORIA,[COLEÇÃO], [GRUPO COLEÇÃO], [CATEGORIA PADRONIZADA]
