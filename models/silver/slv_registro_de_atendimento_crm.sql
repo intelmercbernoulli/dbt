e1e24d397961faeed0879ed76a7e6791d1f63151
Vinicius Gomes Batista <vinicius.batista@bernoulli.com.br>
2026-02-23 17:12:01 -0300

    Atualização

--- models/silver/slv_registro_de_atendimento_crm.sql
+++ models/silver/slv_registro_de_atendimento_crm.sql
@@ -1,10 +1,24 @@
-SELECT *, 
-CASE 
-    WHEN [Status do Atendimento] = 'Planejado' THEN 0
-    WHEN [Status do Atendimento]  = 'Agendado' THEN 1
-    WHEN [Status do Atendimento]  = 'Realizado' THEN 2
-    WHEN [Status do Atendimento]  = 'Cancelado' THEN 3
-    ELSE 4
-END AS prioridade_status_atendimento
-FROM
-{{ source ('intel_merc', 'brz_registro_de_atendimento') }}
+SELECT
+
+    {{ dbt_utils.star(
+        from=source('intel_merc', 'brz_registro_de_atendimento'),
+        except=[
+            'Data de Criação',
+            'Hora de Início',
+            'Hora de Término'
+        ]
+    ) }},
+
+    TRY_CONVERT(DATETIME, [Data de Criação], 103)  AS [Data de Criação],
+    TRY_CONVERT(DATETIME, [Hora de Início], 103)   AS [Hora de Início],
+    TRY_CONVERT(DATETIME, [Hora de Término], 103)  AS [Hora de Término],
+
+    CASE 
+        WHEN [Status do Atendimento] = 'Planejado' THEN 0
+        WHEN [Status do Atendimento] = 'Agendado' THEN 1
+        WHEN [Status do Atendimento] = 'Realizado' THEN 2
+        WHEN [Status do Atendimento] = 'Cancelado' THEN 3
+        ELSE 4
+    END AS prioridade_status_atendimento
+
+FROM {{ source('intel_merc', 'brz_registro_de_atendimento') }}
